8f11eae52580a83b2adee5e258356eb9b7bd1a96
dyoni <dyoni5o59192@gmail.com>
2015-09-24 07:17:47 +0000

    first phonebook

---
+++ Phonebook.rb
@@ -0,0 +1,31 @@
+friend=[]
+
+loop do
+    
+print "name?"
+name = gets.chomp
+
+print "phonenumber?"
+phonenumber = gets.chomp
+
+print "gender?"
+gender = gets.chomp
+
+if gender == "male"
+  puts "male"
+elsif gender == "female"
+  puts "female"
+else
+  puts "male"
+end
+
+friend << {name:name, phonenumber:phonenumber, gender:gender}
+
+friend.each do |x|
+  puts "이름 : #u[:name], 전화번호: #u[:phonenumber],  성별: #u[:gender]"
+end
+
+puts "if you want to add mre press any key, unless press '0'"
+  cmd = gets.chomp
+  break if cmd == "0"
+end
